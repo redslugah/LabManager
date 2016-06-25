f6e46bee47a7ca99b6bba15e823d5d33063a487c
Reds <rafael.douglas.1@gmail.com>
2016-06-25 16:27:52 -0300

    Testes

---
+++ Lab Manager/Lab Manager/PwrShell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+
+namespace Lab_Manager
+{
+    class PwrShell
+    {
+        static void main()
+        {
+            Process.Start("cmd.exe");
+            
+        }
+        public void teste()
+        {
+            main();
+        }
+        
+    }
+}
