8387171fa2115ac6282d77da189fc24991381024
Rafa <Rafa@ED_INFO1>
2016-07-06 13:01:10 -0300

    dssda

--- Lab Manager/Lab Manager/PwrShell.cs
+++ Lab Manager/Lab Manager/PwrShell.cs
@@ -15,17 +15,21 @@ namespace Lab_Manager
         {
             ProcessStartInfo cmd = new ProcessStartInfo("cmd");
             Process teste1 = new Process();
-            cmd.WorkingDirectory = @"C:\windows\";
+            string test;
+            cmd.WorkingDirectory = @"C:\";
             cmd.UseShellExecute = false;
             cmd.RedirectStandardInput = true;
             cmd.RedirectStandardOutput = true;
             cmd.RedirectStandardOutput = true;
             teste1.StartInfo=cmd;
             teste1.Start();
-            teste1.StandardInput.WriteLine("net use",@"\\172.30.17.53\ipc$"," /user:educacao ailiram46825");
+            teste1.StandardInput.WriteLine("echo on");
+            teste1.StandardInput.WriteLine("net view");
+            teste1.StandardOutput.ToString();
+            teste1.StandardInput.WriteLine("net use \\172.30.17.53\\ipc$ /user:educacao ailiram46825");
             //teste1.StandardInput.WriteLine("break");
             //teste1.StandardInput.WriteLine("ailiram46825");
-            teste1.StandardInput.WriteLine("shutdown -r -t 5 -m \\172.30.17.53");
+            //teste1.StandardInput.WriteLine("shutdown -r -t 5 -m \\172.30.17.53");
 
 
             //teste1.WaitForExit();// Waits here for the process to exit.
