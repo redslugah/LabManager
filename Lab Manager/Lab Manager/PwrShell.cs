f77944f2168eabe0a478188fa7e2278c9bb30df7
Rafa <Rafa@ED_INFO1>
2016-07-06 11:17:35 -0300

    testando

--- Lab Manager/Lab Manager/PwrShell.cs
+++ Lab Manager/Lab Manager/PwrShell.cs
@@ -13,7 +13,23 @@ namespace Lab_Manager
     {
         static void main()
         {
-            Process.Start("cmd.exe");
+            ProcessStartInfo cmd = new ProcessStartInfo("cmd");
+            Process teste1 = new Process();
+            cmd.WorkingDirectory = @"C:\windows\";
+            cmd.UseShellExecute = false;
+            cmd.RedirectStandardInput = true;
+            cmd.RedirectStandardOutput = true;
+            cmd.RedirectStandardOutput = true;
+            teste1.StartInfo=cmd;
+            teste1.Start();
+            teste1.StandardInput.WriteLine("net use",@"\\172.30.17.53\ipc$"," /user:educacao ailiram46825");
+            //teste1.StandardInput.WriteLine("break");
+            //teste1.StandardInput.WriteLine("ailiram46825");
+            teste1.StandardInput.WriteLine("shutdown -r -t 5 -m \\172.30.17.53");
+
+
+            //teste1.WaitForExit();// Waits here for the process to exit.
+            //Process.Start("cmd.exe");
             
         }
         public void teste()
